2933a2eb6d98e3e7848a8c969af0f96512ced81f
CS Yong <yongchuseng@gmail.com>
2023-01-14 21:15:59 +0800

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div class="links">
+      <a href="#">Meta</a>
+      <a href="#">About</a>
+      <a href="#">Blog</a>
+      <a href="#">Jobs</a>
+      <a href="#">Help</a>
+      <a href="#">API</a>
+      <a href="#">Privacy</a>
+      <a href="#">Terms</a>
+      <a href="#">Top Accounts</a>
+      <a href="#">Locations</a>
+      <a href="#">Instagram Lite</a>
+    </div>
+  );
 }
 
 export default Footer;
