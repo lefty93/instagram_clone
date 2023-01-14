f8d6c830272e0ea6802f038e9e30a02d67430f5c
CS Yong <yongchuseng@gmail.com>
2023-01-14 13:42:25 +0800

    add svg

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -23,7 +23,12 @@ function LoginForm() {
         <div className="form-container">
           <h1>Instagram</h1>
           <form>
-            <input type="email" name="" id="" placeholder="Phone number, username, or email" />
+            <input
+              type="email"
+              name=""
+              id=""
+              placeholder="Phone number, username, or email"
+            />
             <input type="password" name="" id="" placeholder="Password" />
             <button>Log in</button>
             <div className="or">
@@ -31,7 +36,9 @@ function LoginForm() {
               <span>OR</span>
               <hr />
               <div className="facebook">
-
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z" />
+                </svg>
               </div>
             </div>
           </form>
