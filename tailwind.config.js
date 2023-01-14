c62f0503aa1e7bb756dfe32798aefc4b07c62b41
Lefty93 <yongchuseng@gmail.com>
2023-01-14 20:04:45 +0800

    Finish login form

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      engagement: ["Engagement", "cursive"]
+    },
   },
   plugins: [],
 };
